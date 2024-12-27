e957b36cbf574f262382eca0a24e40fd787139de
quperino <vlad.vagapov2003@gmail.com>
2024-12-27 16:30:45 +0300

    Исправил

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -23,12 +23,13 @@ package com.walking.intensive.chapter1.task3;
  *
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
+
 public class Task3 {
     public static void main(String[] args) {
         System.out.println(isLeap(2020));
     }
 
     static boolean isLeap(int year) {
-        return ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) && (year > 0);
+        return (year > 0) && ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0));
     }
 }
