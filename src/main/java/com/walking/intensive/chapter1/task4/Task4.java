8e6dda4312175fc3697a2f72e3285200abdc5003
quperino <vlad.vagapov2003@gmail.com>
2025-01-01 18:39:41 +0300

    Исправил, согласно комментариям. Теперь условия поставлены без опоры на дискриминант, а высчитывается он только по необходимости, т.е. в случае, который не встретился в первых трех проверках

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,27 +24,29 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 3;
-        double b = -14;
-        double c = -5;
+        double a = 0;
+        double b = 0;
+        double c = 0;
         System.out.println(solveEquation(a, b, c));
     }
 
     static String solveEquation(double a, double b, double c) {
-        double discriminant = (Math.pow(b, 2)) - (4 * a * c);
-        String s = "";
+        String s;
 
-        if (discriminant < 0) {
+        if (a == 0 && b == 0 && c == 0) {
+            s = "Бесконечное множество решений.";
+        } else if (a == 0 && b == 0) {
             s = "Количество решений: 0.";
-        } else if (discriminant == 0) {
-            s = "Количество решений: 1. Корень: " + (-b / 2 * a);
-        } else if (discriminant > 0) {
+        } else if (a == 0 && c != 0) {
+            s = "Количество решений: 1. Корень: " + (-c / b);
+        } else {
+            double discriminant = (Math.pow(b, 2)) - (4 * a * c);
             double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
             double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
             if (x1 > x2) {
-                s = "Количество решений: 2. Корни: " + x2 + "; " + x1;
+                s = "Количество решений: 2. Корни: " + x2 + ";" + x1;
             } else {
-                s = "Количество решений: 2. Корни: " + x1 + "; " + x2;
+                s = "Количество решений: 2. Корни: " + x1 + ";" + x2;
             }
         }
 
