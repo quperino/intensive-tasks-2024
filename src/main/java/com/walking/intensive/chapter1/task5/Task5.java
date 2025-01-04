f1e242695268f0cd0940732353b329dc4f9be85d
quperino <vlad.vagapov2003@gmail.com>
2025-01-04 21:47:47 +0300

    Внес правки, согласно комментариям. Вторая попытка Task5.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Задача поиска площади, величин углов, длин высот, биссектрис, медиан, радиусов вписанной и описанной вокруг
  * треугольника окружностей является центральной в Геометрии.
@@ -8,6 +10,7 @@ package com.walking.intensive.chapter1.task5;
  *
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
+
 public class Task5 {
     public static void main(String[] args) {
         System.out.println(getAreaByHeron(2, 3, 4) + "\n");
@@ -45,13 +48,16 @@ public class Task5 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
+
     static double getAreaByHeron(double a, double b, double c) {
         double areaByHeron = -1;
 
-        if (isTriangleExist(a, b, c)) {
-            double semiperimeter = (a + b + c) / 2;
-            areaByHeron = Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+        if (doesTriangleNotExist(a, b, c)) {
+            return areaByHeron;
         }
+
+        double semiperimeter = getSemiperimeter(a, b, c);
+        areaByHeron = Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
 
         return areaByHeron;
     }
@@ -63,30 +69,16 @@ public class Task5 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
+
     static double[] getHeights(double a, double b, double c) {
         double[] heights = new double[0];
 
-        if (isTriangleExist(a, b, c)) {
-            double semiperimeter = (a + b + c) / 2;
-            double heightTo = 2 * Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
-            double heightToA = heightTo / a;
-            double heightToB = heightTo / b;
-            double heightToC = heightTo / c;
-
-            double maxHeight = Math.max(Math.max(heightToA, heightToB), heightToC);
-            double minHeight = Math.min(Math.min(heightToA, heightToB), heightToC);
-            double averageHeight;
-
-            if (heightToA != minHeight && heightToA != maxHeight) {
-                averageHeight = heightToA;
-            } else if (heightToB != minHeight && heightToB != maxHeight) {
-                averageHeight = heightToB;
-            } else {
-                averageHeight = heightToC;
-            }
-
-            heights = new double[]{minHeight, averageHeight, maxHeight};
+        if (doesTriangleNotExist(a, b, c)) {
+            return heights;
         }
+
+        heights = new double[]{2 * getAreaByHeron(a, b, c) / a, 2 * getAreaByHeron(a, b, c) / b, 2 * getAreaByHeron(a, b, c) / c};
+        Arrays.sort(heights);
 
         return heights;
     }
@@ -98,28 +90,16 @@ public class Task5 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
+
     static double[] getMedians(double a, double b, double c) {
         double[] medians = new double[0];
 
-        if (isTriangleExist(a, b, c)) {
-            double medianToA = Math.sqrt(2 * b * b + 2 * c * c - a * a) / 2;
-            double medianToB = Math.sqrt(2 * a * a + 2 * c * c - b * b) / 2;
-            double medianToC = Math.sqrt(2 * a * a + 2 * b * b - c * c) / 2;
-
-            double maxMedian = Math.max(Math.max(medianToA, medianToB), medianToC);
-            double minMedian = Math.min(Math.min(medianToA, medianToB), medianToC);
-            double averageMedian;
-
-            if (medianToA != minMedian && medianToA != maxMedian) {
-                averageMedian = medianToA;
-            } else if (medianToB != minMedian && medianToB != maxMedian) {
-                averageMedian = medianToB;
-            } else {
-                averageMedian = medianToC;
-            }
-
-            medians = new double[]{minMedian, averageMedian, maxMedian};
+        if (doesTriangleNotExist(a, b, c)) {
+            return medians;
         }
+
+        medians = new double[]{Math.sqrt(2 * b * b + 2 * c * c - a * a) / 2, Math.sqrt(2 * a * a + 2 * c * c - b * b) / 2, Math.sqrt(2 * a * a + 2 * b * b - c * c) / 2};
+        Arrays.sort(medians);
 
         return medians;
     }
@@ -134,25 +114,12 @@ public class Task5 {
     static double[] getBisectors(double a, double b, double c) {
         double[] bisectors = new double[0];
 
-        if (isTriangleExist(a, b, c)) {
-            double bisectorToA = Math.sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c);
-            double bisectorToB = Math.sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c);
-            double bisectorToC = Math.sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b);
-
-            double maxBisector = Math.max(Math.max(bisectorToA, bisectorToB), bisectorToC);
-            double minBisector = Math.min(Math.min(bisectorToA, bisectorToB), bisectorToC);
-            double averageBisector;
-
-            if (bisectorToA != minBisector && bisectorToA != maxBisector) {
-                averageBisector = bisectorToA;
-            } else if (bisectorToB != minBisector && bisectorToB != maxBisector) {
-                averageBisector = bisectorToB;
-            } else {
-                averageBisector = bisectorToC;
-            }
-
-            bisectors = new double[]{minBisector, averageBisector, maxBisector};
+        if (doesTriangleNotExist(a, b, c)) {
+            return bisectors;
         }
+
+        bisectors = new double[]{Math.sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c), Math.sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c), Math.sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b)};
+        Arrays.sort(bisectors);
 
         return bisectors;
     }
@@ -167,29 +134,12 @@ public class Task5 {
     static double[] getAngles(double a, double b, double c) {
         double[] angles = new double[0];
 
-        if (isTriangleExist(a, b, c)) {
-            double cosAngleAB = (a * a + b * b - c * c) / 2 * a * b;
-            double cosAngleAC = (a * a + c * c - b * b) / 2 * a * c;
-            double cosAngleBC = (b * b + c * c - a * a) / 2 * b * c;
-
-            double angleAB = Math.acos(cosAngleAB) * 180 / Math.PI;
-            double angleAC = Math.acos(cosAngleAC) * 180 / Math.PI;
-            double angleBC = Math.acos(cosAngleBC) * 180 / Math.PI;
-
-            double maxAngle = Math.max(Math.max(angleAC, angleAB), angleBC);
-            double minAngle = Math.min(Math.min(angleAC, angleAB), angleBC);
-            double averageAngle;
-
-            if (angleAB != minAngle && angleAB != maxAngle) {
-                averageAngle = angleAB;
-            } else if (angleAC != minAngle && angleAC != maxAngle) {
-                averageAngle = angleAC;
-            } else {
-                averageAngle = angleBC;
-            }
-
-            angles = new double[]{minAngle, averageAngle, maxAngle};
+        if (doesTriangleNotExist(a, b, c)) {
+            return angles;
         }
+
+        angles = new double[]{Math.acos((a * a + b * b - c * c) / 2 * a * b) * 180 / Math.PI, Math.acos((a * a + c * c - b * b) / 2 * a * c) * 180 / Math.PI, Math.acos((b * b + c * c - a * a) / 2 * b * c) * 180 / Math.PI};
+        Arrays.sort(angles);
 
         return angles;
     }
@@ -204,10 +154,11 @@ public class Task5 {
     static double getInscribedCircleRadius(double a, double b, double c) {
         double inscribedCircleRadius = -1;
 
-        if (isTriangleExist(a, b, c)) {
-            double semiperimeter = (a + b + c) / 2;
-            inscribedCircleRadius = Math.sqrt((semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c) / semiperimeter);
+        if (doesTriangleNotExist(a, b, c)) {
+            return inscribedCircleRadius;
         }
+
+        inscribedCircleRadius = Math.sqrt(getAreaByHeron(a, b, c) / getSemiperimeter(a, b, c) / getSemiperimeter(a, b, c));
 
         return inscribedCircleRadius;
     }
@@ -222,10 +173,11 @@ public class Task5 {
     static double getCircumradius(double a, double b, double c) {
         double circumradius = -1;
 
-        if (isTriangleExist(a, b, c)) {
-            double semiperimeter = (a + b + c) / 2;
-            circumradius = a * b * c / 4 * Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+        if (doesTriangleNotExist(a, b, c)) {
+            return circumradius;
         }
+
+        circumradius = a * b * c / 4 / getAreaByHeron(a, b, c);
 
         return circumradius;
     }
@@ -247,17 +199,21 @@ public class Task5 {
     static double getAreaAdvanced(double a, double b, double c) {
         double area = -1;
 
-        if (isTriangleExist(a, b, c)) {
-            double cosAngleBC = (b * b + c * c - a * a) / 2 * b * c;
-            double sinAngleBC = Math.sqrt(1 - cosAngleBC);
-            area = b * c * sinAngleBC / 2;
+        if (doesTriangleNotExist(a, b, c)) {
+            return area;
         }
+
+        double sinAngleBC = Math.sqrt(1 - ((b * b + c * c - a * a) / 2 * b * c));
+        area = b * c * sinAngleBC / 2;
 
         return area;
     }
 
-    static boolean isTriangleExist(double a, double b, double c) {
-        return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+    static boolean doesTriangleNotExist(double a, double b, double c) {
+        return !(a > 0) || !(b > 0) || !(c > 0) || !(a + b > c) || !(a + c > b) || !(b + c > a);
     }
 
+    static double getSemiperimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
 }
