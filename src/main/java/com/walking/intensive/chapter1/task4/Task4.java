61e4ff976702d64403429df78fe48976c076af8e
quperino <vlad.vagapov2003@gmail.com>
2025-01-07 00:33:49 +0300

    Полностью переписал код, прошлые комментарии к старому коду учел

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,9 +24,9 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 4;
-        double b = 4;
-        double c = 1;
+        double a = -1;
+        double b = -3;
+        double c = 0;
         System.out.println(solveEquation(a, b, c));
     }
 
@@ -35,19 +35,24 @@ public class Task4 {
 
         if (a == 0 && b == 0 && c == 0) {
             s = "Бесконечное множество решений.";
-        } else if (b * b < 4 * a * c) {
+        } else if (a == 0 && b == 0 || a > 0 && c > 0 && b == 0 || a < 0 && c < 0 && b == 0 || b * b < 4 * a * c) {
             s = "Количество решений: 0.";
+        } else if (a == 0 && c == 0 || b == 0 && c == 0) {
+            s = "Количество решений: 1. Корень: 0";
+        } else if (a == 0 && b < 0 && c < 0 || a == 0 && b < 0 && c > 0 || a == 0 && b > 0 && c < 0 || a == 0 && b > 0 && c > 0){
+            s = "Количество решений: 1. Корень: " + -c / b;
         } else if (b * b == 4 * a * c) {
-            s = "Количество решений: 1. Корень: " + (-c / b);
+            s = "Количество решений: 1. Корень: " + -b / 2 * a;
+        } else if (a > 0 && c < 0 || a < 0 && c > 0) {
+            double x1 = Math.sqrt(c / a);
+            double x2 = -Math.sqrt(c / a);
+            s = "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
+        } else if (a != 0 && b != 0 && c == 0) {
+            s = "Количество решений: 2. Корни: " + Math.min(0, -b / a) + ";" + Math.max(0, -b / a);
         } else {
-            double discriminant = (Math.pow(b, 2)) - (4 * a * c);
-            double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            if (x1 > x2) {
-                s = "Количество решений: 2. Корни: " + x2 + ";" + x1;
-            } else {
-                s = "Количество решений: 2. Корни: " + x1 + ";" + x2;
-            }
+            double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+            double x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+            s = "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
         }
 
         return s;
