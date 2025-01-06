1c0c3d1cecf2f6a1fe34bece937f9eef3babe2d2
quperino <vlad.vagapov2003@gmail.com>
2025-01-06 22:43:51 +0300

    Исправил условия, согласно комментариям

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -1,18 +1,17 @@
 package com.walking.intensive.chapter2.task6;
 
-import java.util.Arrays;
-
 /**
  * Реализуйте представленные ниже методы для расчета
  * НОК (наименьшее общее кратное) и НОД (наибольший общий делитель).
  *
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
+
 public class Task6 {
     public static void main(String[] args) {
-        System.out.println(getLcm(70, 140));
-        System.out.println(getGcd(70, 0));
-        System.out.println(getGcdByEuclideanAlgorithm(70, 140));
+        System.out.println(getLcm(30, 160));
+        System.out.println(getGcd(30, 160));
+        System.out.println(getGcdByEuclideanAlgorithm(30, 160));
     }
 
     /**
@@ -22,43 +21,64 @@ public class Task6 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
+
     static int getLcm(int m, int n) {
         int lcm = -1;
 
-        if (m > 0 && n > 0) {
-            int minNumber = Math.min(m, n);
-            int maxNumber = Math.max(m, n);
-            lcm = maxNumber;
-            int[] minNumberFactors = getNumberFactors(minNumber);
-            int[] maxNumberFactors = getNumberFactors(maxNumber);
+        if (m <= 0 || n <= 0) {
+            return lcm;
+        }
 
-            int[] uniqueNumbers = Arrays.stream(minNumberFactors).distinct().toArray();
+        int minNumber = Math.min(m, n);
+        int maxNumber = Math.max(m, n);
+        lcm = maxNumber;
+        int[] minNumberFactors = getNumberFactors(minNumber);
+        int[] maxNumberFactors = getNumberFactors(maxNumber);
 
-            for (int uniqueNumber : uniqueNumbers) {
-                int maxCounter = 0;
-                int minCounter = 0;
+        int[] uniqueNumbersMin = getUniqueNumbers(minNumberFactors);
 
-                if (Arrays.asList(Arrays.stream(maxNumberFactors).boxed().toArray()).contains(uniqueNumber)) {
-                    for (int array : maxNumberFactors) {
-                        if (uniqueNumber == array) {
-                            maxCounter++;
-                        }
-                    }
+        for (int uniqueNumber : uniqueNumbersMin) {
+            int maxCounter = 0;
+            int minCounter = 0;
+
+            boolean isContains = false;
+
+            for (int i : maxNumberFactors) {
+                if (i == uniqueNumber) {
+                    isContains = true;
+                    break;
                 }
-
-                if (Arrays.asList(Arrays.stream(minNumberFactors).boxed().toArray()).contains(uniqueNumber)) {
-                    for (int array : minNumberFactors) {
-                        if (uniqueNumber == array) {
-                            minCounter++;
-                        }
-                    }
-                }
-
-                if (minCounter > maxCounter) {
-                    lcm *= (int) Math.pow(uniqueNumber, (minCounter - maxCounter));
-                }
-
             }
+
+            if (isContains) {
+                for (int array : maxNumberFactors) {
+                    if (uniqueNumber == array) {
+                        maxCounter++;
+                    }
+                }
+            }
+
+            isContains = false;
+
+            for (int i : minNumberFactors) {
+                if (i == uniqueNumber) {
+                    isContains = true;
+                    break;
+                }
+            }
+
+            if (isContains) {
+                for (int array : minNumberFactors) {
+                    if (uniqueNumber == array) {
+                        minCounter++;
+                    }
+                }
+            }
+
+            if (minCounter > maxCounter) {
+                lcm *= (int) Math.pow(uniqueNumber, (minCounter - maxCounter));
+            }
+
         }
 
         return lcm;
@@ -71,43 +91,64 @@ public class Task6 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
+
     static int getGcd(int m, int n) {
         int gcd = -1;
 
-        if (m > 0 && n > 0) {
-            int[] numberFactorsForM = getNumberFactors(m);
-            int[] numberFactorsForN = getNumberFactors(n);
-            gcd = 1;
+        if (m <= 0 || n <= 0) {
+            return gcd;
+        }
 
-            int[] uniqueNumbersForM = Arrays.stream(numberFactorsForM).distinct().toArray();
+        int[] numberFactorsForM = getNumberFactors(m);
+        int[] numberFactorsForN = getNumberFactors(n);
+        gcd = 1;
 
-            for (int uniqueNumberForM : uniqueNumbersForM) {
-                int counterForM = 0;
-                int counterForN = 0;
+        int[] uniqueNumbersForM = getUniqueNumbers(numberFactorsForM);
 
-                if (Arrays.asList(Arrays.stream(numberFactorsForM).boxed().toArray()).contains(uniqueNumberForM)) {
-                    for (int array : numberFactorsForM) {
-                        if (uniqueNumberForM == array) {
-                            counterForM++;
-                        }
-                    }
-                } else {
-                    continue;
+        for (int uniqueNumberForM : uniqueNumbersForM) {
+            int counterForM = 0;
+            int counterForN = 0;
+
+            boolean isContains = false;
+
+            for (int i : numberFactorsForM) {
+                if (i == uniqueNumberForM) {
+                    isContains = true;
+                    break;
                 }
-
-                if (Arrays.asList(Arrays.stream(numberFactorsForN).boxed().toArray()).contains(uniqueNumberForM)) {
-                    for (int array : numberFactorsForN) {
-                        if (uniqueNumberForM == array) {
-                            counterForN++;
-                        }
-                    }
-                } else {
-                    continue;
-                }
-
-                int minOfCounters = Math.min(counterForM, counterForN);
-                gcd *= (int) Math.pow(uniqueNumberForM, minOfCounters);
             }
+
+            if (isContains) {
+                for (int array : numberFactorsForM) {
+                    if (uniqueNumberForM == array) {
+                        counterForM++;
+                    }
+                }
+            } else {
+                continue;
+            }
+
+            isContains = false;
+
+            for (int i : numberFactorsForN) {
+                if (i == uniqueNumberForM) {
+                    isContains = true;
+                    break;
+                }
+            }
+
+            if (isContains) {
+                for (int array : numberFactorsForN) {
+                    if (uniqueNumberForM == array) {
+                        counterForN++;
+                    }
+                }
+            } else {
+                continue;
+            }
+
+            int minOfCounters = Math.min(counterForM, counterForN);
+            gcd *= (int) Math.pow(uniqueNumberForM, minOfCounters);
         }
 
         return gcd;
@@ -121,37 +162,29 @@ public class Task6 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
+
     static int getGcdByEuclideanAlgorithm(int m, int n) {
         int gcd = -1;
-
-        if (m > 0 && n > 0) {
-            int minNumber = Math.min(m, n);
-            int maxNumber = Math.max(m, n);
-            gcd = minNumber;
-            int maxNumberBuffer;
-
-            while (minNumber != 0) {
-                if (maxNumber % minNumber != 0) {
-                    gcd = maxNumber % minNumber;
-                }
-                maxNumberBuffer = maxNumber;
-                maxNumber = minNumber;
-                minNumber = maxNumberBuffer % minNumber;
-            }
+        if (m <= 0 || n <= 0) {
+            return gcd;
         }
 
+        gcd = getGcdByEuclideanAlgorithmCalculation(m, n);
         return gcd;
     }
 
+
+    // Custom methods
+
     static int[] getNumberFactors(int number) {
-        StringBuilder numberFactorsStringBuilder = new StringBuilder();
         int k = 2;
+        int arrayLength = 0;
+        int numberBackUp = number;
 
         while (number != 1) {
-
             if (number % k == 0) {
 
-                numberFactorsStringBuilder.append(k).append(";");
+                arrayLength++;
                 number = number / k;
                 k = 2;
 
@@ -160,14 +193,53 @@ public class Task6 {
             }
         }
 
-        String numberFactorsString = numberFactorsStringBuilder.toString();
-        String[] numberFactorsStringArray = numberFactorsString.split(";");
-        int[] numberFactorsInt = new int[numberFactorsStringArray.length];
+        int[] numbersFactors = new int[arrayLength];
 
-        for (int i = 0; i < numberFactorsStringArray.length; i++) {
-            numberFactorsInt[i] = Integer.parseInt(numberFactorsStringArray[i]);
+        int i = -1;
+        while (numberBackUp != 1) {
+            if (numberBackUp % k == 0) {
+                i++;
+                numbersFactors[i] = k;
+                numberBackUp = numberBackUp / k;
+                k = 2;
+
+            } else {
+                k++;
+            }
         }
 
-        return numberFactorsInt;
+        return numbersFactors;
+    }
+
+    static int[] getUniqueNumbers(int[] array) {
+        int k = 1;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1]) {
+                k++;
+            }
+        }
+
+        int[] uniqueNumbers = new int[k];
+        k = 0;
+        uniqueNumbers[0] = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1]) {
+                k++;
+                uniqueNumbers[k] = array[i];
+            }
+        }
+
+        return uniqueNumbers;
+    }
+
+    static int getGcdByEuclideanAlgorithmCalculation(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+
+        return getGcdByEuclideanAlgorithmCalculation(n, m % n);
+
     }
 }
