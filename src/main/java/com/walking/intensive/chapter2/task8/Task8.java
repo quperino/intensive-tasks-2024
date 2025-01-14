83e13e300cc2c1df1fc0309d02b7109e11db7319
quperino <vlad.vagapov2003@gmail.com>
2025-01-14 06:39:46 +0300

    Первая попытка решения Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -18,13 +18,32 @@ package com.walking.intensive.chapter2.task8;
  *
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
+
 public class Task8 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        int happyTicketCounter = 0;
+        int[] array = new int[6];
+
+        for (int i = 0; i <= 999_999; i++) {
+            int k = i;
+            int leftNumbersSum = 0;
+            int rightNumbersSum = 0;
+
+            for (int j = 5; j >= 0; j--) {
+                array[j] = k % 10;
+                k /= 10;
+
+                if (j > 2) rightNumbersSum += array[j];
+                else leftNumbersSum += array[j];
+            }
+
+            if (leftNumbersSum == rightNumbersSum) happyTicketCounter++;
+        }
+
+        return (double) happyTicketCounter / 1_000_000;
     }
 }
